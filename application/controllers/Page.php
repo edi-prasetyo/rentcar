196d7fe1e0cea3d1724be27d781ae4d93df2d3f2
Edi Prasetyi <ediprasetiyo2@gmail.com>
2022-04-26 14:04:14 +0700

    Update Page

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -22,27 +22,15 @@ class Page extends CI_Controller
     {
         $meta           = $this->meta_model->get_meta();
         $page           = $this->page_model->get_page();
-        if (!$this->agent->is_mobile()) {
-            // Desktop View
-            $data = array(
-                'title'       => 'Halaman',
-                'deskripsi'   => 'Berita - ' . $meta->description,
-                'keywords'    => 'Berita - ' . $meta->keywords,
-                'page'        => $page,
-                'content'     => 'front/page/index_page'
-            );
-            $this->load->view('front/layout/wrapp', $data, FALSE);
-        } else {
-            // Mobile View
-            $data = array(
-                'title'       => 'Halaman',
-                'deskripsi'   => 'Berita - ' . $meta->description,
-                'keywords'    => 'Berita - ' . $meta->keywords,
-                'page'        => $page,
-                'content'     => 'mobile/page/index'
-            );
-            $this->load->view('mobile/layout/wrapp', $data, FALSE);
-        }
+
+        $data = array(
+            'title'       => 'Halaman',
+            'deskripsi'   => 'Berita - ' . $meta->description,
+            'keywords'    => 'Berita - ' . $meta->keywords,
+            'page'        => $page,
+            'content'     => 'front/page/index_page'
+        );
+        $this->load->view('front/layout/wrapp', $data, FALSE);
     }
 
     public function detail($page_slug)
@@ -50,26 +38,14 @@ class Page extends CI_Controller
         $meta           = $this->meta_model->get_meta();
         $page           = $this->page_model->read($page_slug);
 
-        if (!$this->agent->is_mobile()) {
-            // Desktop View
-            $data = array(
-                'title'       => $page->page_title,
-                'deskripsi'   => 'Halaman - ' . $meta->description,
-                'keywords'    => 'Halaman - ' . $meta->keywords,
-                'page'        => $page,
-                'content'     => 'front/page/detail_page'
-            );
-            $this->load->view('front/layout/wrapp', $data, FALSE);
-        } else {
-            // Mobile View
-            $data = array(
-                'title'       => $page->page_title,
-                'deskripsi'   => 'Halaman - ' . $meta->description,
-                'keywords'    => 'Halaman - ' . $meta->keywords,
-                'page'        => $page,
-                'content'     => 'mobile/page/detail'
-            );
-            $this->load->view('mobile/layout/wrapp', $data, FALSE);
-        }
+
+        $data = array(
+            'title'       => $page->page_title,
+            'deskripsi'   => 'Halaman - ' . $meta->description,
+            'keywords'    => 'Halaman - ' . $meta->keywords,
+            'page'        => $page,
+            'content'     => 'front/page/detail_page'
+        );
+        $this->load->view('front/layout/wrapp', $data, FALSE);
     }
 }
