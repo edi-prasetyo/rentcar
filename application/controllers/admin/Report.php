ed86f73c020562cb8e90684f9212b94780de476a
Edi Prasetyi <ediprasetiyo2@gmail.com>
2022-12-09 23:54:54 +0700

    Update Report

--- application/controllers/admin/Report.php
+++ application/controllers/admin/Report.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-// require('./application/third_party/phpoffice/autoload.php');
+require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
