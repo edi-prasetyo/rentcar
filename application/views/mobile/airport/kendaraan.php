029bde3bce63ea0b0d67fd34d65d5ffb7a220d97
edi-prasetyo <ediprasetiyo2@gmail.com>
2024-01-22 12:12:51 +0700

    update Paket Price

--- application/views/mobile/airport/kendaraan.php
+++ application/views/mobile/airport/kendaraan.php
@@ -24,34 +24,37 @@
 
     <div class="mb-5 pb-3">
         <?php foreach ($paket_airport as $data) : ?>
+            <?php if ($data->mobil_gambar == null) : ?>
+            <?php else : ?>
 
-            <div class="card shadow border-0 mb-3">
-                <div class="row">
-                    <div class="col-5 border-right">
-                        <div class="card-body">
-                            <img src="<?php echo base_url('assets/img/mobil/' . $data->mobil_gambar); ?>" class="card-img-top" alt="...">
-                            <div class="text-center"><i class="fa fa-user"></i> <?php echo $data->mobil_penumpang; ?> <i class="fa fa-briefcase ml-3"></i> <?php echo $data->mobil_bagasi; ?></div>
+                <div class="card shadow border-0 mb-3">
+                    <div class="row">
+                        <div class="col-5 border-right">
+                            <div class="card-body">
+                                <img src="<?php echo base_url('assets/img/mobil/' . $data->mobil_gambar); ?>" class="card-img-top" alt="...">
+                                <div class="text-center"><i class="fa fa-user"></i> <?php echo $data->mobil_penumpang; ?> <i class="fa fa-briefcase ml-3"></i> <?php echo $data->mobil_bagasi; ?></div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-7">
-                        <div class="card-body text-center">
-                            <h6><?php echo $data->mobil_name; ?></h6>
-                            <h4>Rp. <?php echo number_format($data->paket_price, 0, ",", "."); ?></h4>
-                            <?php echo form_open('airport/order/', array('method' => 'get')); ?>
-                            <input type="hidden" name="mobil_name" value="<?php echo $data->mobil_name; ?>">
-                            <input type="hidden" name="mobil_id" value="<?php echo $data->mobil_id; ?>">
-                            <input type="hidden" name="tanggal_sewa" value="<?php echo $tanggal_sewa; ?>">
-                            <input type="hidden" name="jam_jemput" value="<?php echo $jam_jemput; ?>">
-                            <input type="hidden" name="airport_id" value="<?php echo $airport_id; ?>">
-                            <input type="hidden" name="kota_id" value="<?php echo $kota_id; ?>">
-                            <input type="hidden" name="airport_name" value="<?php echo $airport_name; ?>">
-                            <input type="hidden" name="kota_name" value="<?php echo $kota_name; ?>">
-                            <button type="submit" class="btn btn-sm btn-primary btn-block">Pilih</button>
-                            <?php echo form_close(); ?>
+                        <div class="col-7">
+                            <div class="card-body text-center">
+                                <h6><?php echo $data->mobil_name; ?></h6>
+                                <h4>Rp. <?php echo number_format($data->paket_price, 0, ",", "."); ?></h4>
+                                <?php echo form_open('airport/order/', array('method' => 'get')); ?>
+                                <input type="hidden" name="mobil_name" value="<?php echo $data->mobil_name; ?>">
+                                <input type="hidden" name="mobil_id" value="<?php echo $data->mobil_id; ?>">
+                                <input type="hidden" name="tanggal_sewa" value="<?php echo $tanggal_sewa; ?>">
+                                <input type="hidden" name="jam_jemput" value="<?php echo $jam_jemput; ?>">
+                                <input type="hidden" name="airport_id" value="<?php echo $airport_id; ?>">
+                                <input type="hidden" name="kota_id" value="<?php echo $kota_id; ?>">
+                                <input type="hidden" name="airport_name" value="<?php echo $airport_name; ?>">
+                                <input type="hidden" name="kota_name" value="<?php echo $kota_name; ?>">
+                                <button type="submit" class="btn btn-sm btn-primary btn-block">Pilih</button>
+                                <?php echo form_close(); ?>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            <?php endif; ?>
 
         <?php endforeach; ?>
     </div>
